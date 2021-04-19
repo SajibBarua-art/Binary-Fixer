280b4a60e0d5d1751f8bc819eba4459c712e3853
SajibBarua-art <sajibbaruanstu@gmail.com>
2021-04-20 00:39:37 +0600

    done

--- src/Components/ServiceState/ServiceState.js
+++ src/Components/ServiceState/ServiceState.js
@@ -4,11 +4,11 @@ import { useForm } from "react-hook-form";
 const ServiceState = ({ setStatus, id }) => {
     const { register, handleSubmit } = useForm();
 
-    const onSubmit = (data) => {
+    const onSubmit = (status) => {
         fetch(`/updateStatus/${id}`, {
             method: 'PATCH',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ status: data })
+            body: JSON.stringify({ status })
         })
             .then(res => res.json())
             .then(currentStatus => setStatus(currentStatus))
